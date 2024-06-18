36ea33526d8398c438fe6b80fb7e215efe1d14e7
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-06-18 15:50:44 +0300

    fix tailwind.config, fix general body css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      mainblue: "#23A6F0",
+      papayas: {
+        textblack: "#252B42",
+        textgray: "#737373",
+        textwhite: "#FFFFFF",
+      },
+    },
+    fontFamily: {
+      sans: ["Montserrat"],
+      serif: ["Montserrat"],
+      mono: ["Montserrat"],
+    },
   },
   plugins: [],
 };
