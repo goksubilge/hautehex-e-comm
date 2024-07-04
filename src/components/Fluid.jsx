f7b8c49a6b4746e848094ba7745fe709e95c3189
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-07-04 18:15:25 +0300

    edit Fluid

--- src/components/Fluid.jsx
+++ src/components/Fluid.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 function Fluid() {
   return (
-    <div className="flex flex-col items-center gap-8">
-      <div className="text-center gap-8 mt-32 mb-20 max-w-64">
+    <div className="flex flex-col justify-between items-center mt-32">
+      <div className="flex flex-col text-center max-w-64 gap-8 mb-8">
         <h5 className=" text-[#BDBDBD]">SUMMER 2020</h5>
         <h2>Part of the Neural Universe</h2>
-        <h4>
+        <h4 className="text-mainSpace-grey">
           We know how large objects will act, but things on a small scale.
         </h4>
       </div>
-      <div className="flex flex-col gap-6 ">
-        <button className="button-std">BUY NOW</button>
-        <button className="button-std-reverse">Learn More</button>
+      <div className="flex flex-col justify-between items-center gap-8">
+        <button className="button-std max-w-fit">BUY NOW</button>
+        <button className="button-std-reverse max-w-fit">Learn More</button>
       </div>
       <div className="flex items-center">
         <img src="../images/Fluid.png" alt="hero" />
