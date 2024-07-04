91b798a4356cca0484ecd03859cf5a3f1c479c70
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-07-04 21:48:05 +0300

    done blog PREview

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,7 +8,7 @@ import {
 
 function Blog() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-between items-center py-20 tracking-normal ">
       <div
         className="flex flex-col text-center gap-3 max-w-52"
         aria-label="editors-pick-title"
@@ -21,52 +21,59 @@ function Blog() {
         </p>
       </div>
 
-      <div>
+      <div className="pt-20">
         <img src={BlogOne1} alt="Blog" />
-      </div>
-      <div className="flex flex-row justify-start  font-thin text-sm text-mainSpace-grey    leading-1 gap-4">
-        <p href="/" className="hover">
-          Google
-        </p>
-        <p href="/" className="hover">
-          Tranding
-        </p>
-        <p href="/" className="hover">
-          New
-        </p>
-      </div>
-      <div>
-        <h4>Loudest à la Madison #1 (Le integral)</h4>
-        <p>
-          We focus on ergonomics and meeting you where you work. It's only a
-          keystroke away.
-        </p>
-        <div className="flex justify-between py-4">
-          <p className="text-mainSpace-grey">
-            <FontAwesomeIcon
-              icon={faClock}
-              size="sm"
-              style={{ color: "#4a4e69" }}
-            />
-            22 April 2021
-          </p>
-          <p className="text-mainSpace-grey">
-            <FontAwesomeIcon
-              icon={faChartLine}
-              size="sm"
-              style={{ color: "#9a8c98" }}
-            />
-            10 comments
-          </p>
+
+        <div className="p-6">
+          <nav className="flex flex-row items-start text-sm text-mainSpace-grey leading-1 gap-4">
+            <p href="/" className="hover">
+              Google
+            </p>
+            <p href="/" className="hover">
+              Tranding
+            </p>
+            <p href="/" className="hover">
+              New
+            </p>
+          </nav>
+          <div>
+            <h4>
+              Loudest à la Madison #1 <p className="block">(L'integral)</p>
+            </h4>
+            <p>
+              We focus on ergonomics and meeting you where you work. It's only a
+              keystroke away.
+            </p>
+            <div className="flex justify-between py-4">
+              <p className="text-mainSpace-grey">
+                <FontAwesomeIcon
+                  icon={faClock}
+                  fade
+                  size="sm"
+                  style={{ color: "#4a4e69" }}
+                />
+                22 April 2021
+              </p>
+              <p className="text-mainSpace-grey">
+                <FontAwesomeIcon
+                  icon={faChartLine}
+                  fade
+                  size="sm"
+                  style={{ color: "#4a4e69" }}
+                />
+                10 comments
+              </p>
+            </div>
+            <div className="inline gap-2 font-bold text-lg text-mainSpace-blue">
+              Learn More
+              <FontAwesomeIcon
+                icon={faAngleRight}
+                size="lg"
+                style={{ color: "#4a4e69" }}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="inline gap-2 font-bold text-lg text-mainSpace-blue">
-        Learn More
-        <FontAwesomeIcon
-          icon={faAngleRight}
-          size="lg"
-          style={{ color: "#4a4e69" }}
-        />
       </div>
     </div>
   );
