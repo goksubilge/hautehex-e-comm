397930966e2744231b85bec5024cad8e065968e4
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-07-05 10:46:12 +0300

    T2: Home Page Fixes #3 Create BlogData

---
+++ src/mocks/BlogData.jsx
@@ -0,0 +1,39 @@
+import BlogOne1 from "/images/BlogOne1.jpeg";
+import BlogOne2 from "/images/BlogOne2.jpeg";
+import BlogOne3 from "/images/BlogOne3.jpeg";
+const Slider = () => {
+  return [
+    {
+      id: 0,
+      picture: BlogOne1,
+      title: "Koudetat à la Maison #1 (L'intégrale)",
+      description:
+        "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+      date: "22 April 2021",
+      comments: "10 comments",
+      moreInfo: "Learn More",
+    },
+    {
+      id: 1,
+      picture: BlogOne2,
+      title: "Koudetat à la Maison #1 (L'intégrale)",
+      description:
+        "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+      date: "22 April 2021",
+      comments: "10 comments",
+      moreInfo: "Learn More",
+    },
+    {
+      id: 2,
+      picture: BlogOne3,
+      title: "Koudetat à la Maison #1 (L'intégrale)",
+      description:
+        "We focus on ergonomics and meeting you where you work. It's only a keystroke away.",
+      date: "22 April 2021",
+      comments: "10 comments",
+      moreInfo: "Learn More",
+    },
+  ];
+};
+
+export default Slider;
