6cf5ecff9d153a89245f8e1acc19996b2e1a92ed
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-08-01 23:20:47 +0300

    edit productCardAOne, add photos to ProductCardAOne

--- src/mocks/ProductCardAData.jsx
+++ src/mocks/ProductCardAData.jsx
@@ -5,6 +5,10 @@ import ProductCardAOne4 from "/images/ProductCardAOne4.jpeg";
 import ProductCardAOne5 from "/images/ProductCardAOne5.jpeg";
 import ProductCardAOne6 from "/images/ProductCardAOne6.jpeg";
 import ProductCardAOne7 from "/images/ProductCardAOne7.jpeg";
+import ProductCardAOne8 from "/images/ProductCardAOne8.jpeg";
+import ProductCardAOne9 from "/images/ProductCardAOne9.jpeg";
+import ProductCardAOne10 from "/images/ProductCardAOne10.jpeg";
+import ProductCardAOne11 from "/images/ProductCardAOne11.jpeg";
 
 const ProductCardAData = () => {
   return [
@@ -73,7 +77,43 @@ const ProductCardAData = () => {
     },
     {
       id: 7,
-      picture: ProductCardAOne7,
+      picture: ProductCardAOne8,
+      title: "Graphic Design",
+      description: "English Department",
+      emptyInfo: ["react", "redux", "axios"],
+      price1: "$16.48",
+      price2: "$10.48",
+    },
+    {
+      id: 8,
+      picture: ProductCardAOne9,
+      title: "Graphic Design",
+      description: "English Department",
+      emptyInfo: ["react", "redux", "axios"],
+      price1: "$16.48",
+      price2: "$10.48",
+    },
+    {
+      id: 9,
+      picture: ProductCardAOne10,
+      title: "Graphic Design",
+      description: "English Department",
+      emptyInfo: ["react", "redux", "axios"],
+      price1: "$16.48",
+      price2: "$10.48",
+    },
+    {
+      id: 10,
+      picture: ProductCardAOne11,
+      title: "Graphic Design",
+      description: "English Department",
+      emptyInfo: ["react", "redux", "axios"],
+      price1: "$16.48",
+      price2: "$10.48",
+    },
+    {
+      id: 11,
+      picture: ProductCardAOne1,
       title: "Graphic Design",
       description: "English Department",
       emptyInfo: ["react", "redux", "axios"],
